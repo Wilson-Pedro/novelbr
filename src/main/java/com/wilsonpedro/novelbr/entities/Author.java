edd4d79843314ddd94f9763c843fdfe2b6efb3a9
Wilson-Pedro <wilsonpedroneto@gmail.com>
2024-07-04 08:52:58 -0300

    feat: H2 Database and User

---
+++ src/main/java/com/wilsonpedro/novelbr/entities/Author.java
@@ -0,0 +1,8 @@
+package com.wilsonpedro.novelbr.entities;
+
+public class Author extends User{
+	private static final long serialVersionUID = 1L;
+	
+	private String novel;
+
+}
---
+++ src/main/java/com/wilsonpedro/novelbr/entities/User.java
@@ -0,0 +1,73 @@
+package com.wilsonpedro.novelbr.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "TBL_USER")
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String pseudonym;
+	
+	private String email;
+	
+	private String password;
+	
+	public User() {
+	}
+	
+	public User(String pseudonym) {
+		this.pseudonym = pseudonym;
+	}
+
+	public String getPseudonym() {
+		return pseudonym;
+	}
+
+	public void setPseudonym(String pseudonym) {
+		this.pseudonym = pseudonym;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, password, pseudonym);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(password, other.password)
+				&& Objects.equals(pseudonym, other.pseudonym);
+	}
+}
