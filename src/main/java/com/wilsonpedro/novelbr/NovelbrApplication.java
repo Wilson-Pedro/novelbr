6489ac29bf4515a13bb83facb8c492b061872098
Wilson-Pedro <wilsonpedroneto@gmail.com>
2024-07-04 08:03:11 -0300

    first commit

---
+++ src/main/java/com/wilsonpedro/novelbr/NovelbrApplication.java
@@ -0,0 +1,13 @@
+package com.wilsonpedro.novelbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelbrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilsonpedro/novelbr/NovelbrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilsonpedro.novelbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
